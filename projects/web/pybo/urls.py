c2ef077a434dc802e3a3697269de5989f3dcfe6e
LeeHeonWoo1 <gjsdn245@naver.com>
2023-05-18 20:42:06 +0900

    url별칭 사용하기

--- projects/web/pybo/urls.py
+++ projects/web/pybo/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name = 'pybo'
+
 urlpatterns = [
-  path('', views.index),
-  path('<int:question_id>/', views.detail),
-  
+  # url 별칭
+  # 실무에서 url리팩토링은 빈번하게 일어난다.(ex. /index/question/2 -> index/2/question)
+  # 이렇게 되면 url을 수정할 때 마다 해당되는 url을 직접 찾아서 수정해야 하지만 이는 일부 리스크가 있다.
+  # 이러한 현상을 방지하기 위해 name이라는 옵션을 사용한다.
+  # 하지만 만약 pybo앱 이외에 다른 앱이 프로젝트에 추가되어 같은 별칭명을 사용하게 되면 이는 중복이 발생하기에, 네임스페이스를 의미하는 app_name변수를 위에 작성한다.
+
+  path('', views.index, name='index'), # index라는 별칭 부여
+  path('<int:question_id>/', views.detail, name='detail'), # detail이라는 별칭 부여
 ]
