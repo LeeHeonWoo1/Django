7dfaf60cf6d8d0589a1363289609e99ca32d2eb7
LeeHeonWoo1 <gjsdn245@naver.com>
2023-05-18 18:58:17 +0900

    상세화면 함수 추가

--- projects/web/pybo/views.py
+++ projects/web/pybo/views.py
@@ -1,4 +1,16 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Question
 
 def index(request):
-  return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+  # order_by : 조회 결과 정렬함수. create_date앞에 -가 붙었기에 역순으로 정렬하는것을 의미한다.
+  question_list = Question.objects.order_by('-create_date')
+  context = {'question_list':question_list}
+  # render함수는 질문 목록으로 조회한 question_list데이터를 pybo/question_list.html 파일에 적용하여 HTML을 생성한 후 리턴한다.
+  # pybo/question_list.html => 템플릿 파일.
+  return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+  question = Question.objects.get(id=question_id)
+  context = {'question':question}
+  
+  return render(request, 'pybo/question_detail.html', context)
