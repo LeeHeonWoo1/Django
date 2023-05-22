293270c3be32d5338481a3b947fa15106b59a07c
LeeHeonWoo1 <gjsdn245@naver.com>
2023-05-22 17:34:34 +0900

    로그인 필요 어노테이션 추가

--- projects/web/pybo/views.py
+++ projects/web/pybo/views.py
@@ -4,6 +4,7 @@ from .models import Question
 from .forms import QuestionForm, AnswerForm
 from django.http import HttpResponseNotAllowed
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 def index(request):
   page = request.GET.get('page', '1') # 페이지
@@ -22,12 +23,14 @@ def detail(request, question_id):
   
   return render(request, 'pybo/question_detail.html', context)
 
+@login_required(login_url="common:login")
 def answer_create(request, question_id):
   question = get_object_or_404(Question, pk=question_id)
   if request.method == "POST":
     form = AnswerForm(request.POST)
     if form.is_valid():
       answer = form.save(commit=False)
+      answer.author = request.user # author 속성에 로그인 계정 저장
       answer.create_date = timezone.now()
       answer.question = question
       answer.save()
@@ -38,12 +41,14 @@ def answer_create(request, question_id):
   context = {'question':question, 'form':form}
   return render(request, 'pybo/question_detail.html', context)
 
+@login_required(login_url="common:login")
 def question_create(request):
   form = QuestionForm()
   if request.method == 'POST':
     form = QuestionForm(request.POST)
     if form.is_valid():
       question = form.save(commit=False)
+      question.author = request.user # author 속성에 로그인 계정 저장
       question.create_date = timezone.now()
       question.save()
       return redirect('pybo:index')
