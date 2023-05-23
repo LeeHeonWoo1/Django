9bbc72c5180f009de018c57dd92c9491cdaba9a0
LeeHeonWoo1 <gjsdn245@naver.com>
2023-05-23 16:19:59 +0900

    답변 수정, 삭제 url패턴 추가

--- projects/web/pybo/urls.py
+++ projects/web/pybo/urls.py
@@ -15,5 +15,8 @@ urlpatterns = [
   path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'), # answer_create라는 별칭 부여
   path('question/create/', views.question_create, name='question_create'),
   path('question/modify/<int:question_id>/', views.question_modify, name='question_modify'),
-  path('question/delete/<int:question_id>/', views.question_delete, name='question_delete')
+  path('question/delete/<int:question_id>/', views.question_delete, name='question_delete'),
+  path('answer/modify/<int:answer_id>/', views.answer_modify, name='answer_modify'),
+  path('answer/delete/<int:answer_id>/', views.answer_delete, name='answer_delete'),
+  
 ]
