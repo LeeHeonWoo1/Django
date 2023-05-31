09d63cb179a49f0f4261f958a665dbdf61909b5f
LeeHeonWoo1 <gjsdn245@naver.com>
2023-05-31 22:11:44 +0900

    구직정보 리스트 화면 전송함수 작성

--- projects/sideProject/goot_job/views.py
+++ projects/sideProject/goot_job/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from functions.croller_bs4 import get_job_title
 
-# Create your views here.
 def index(request):
   return render(request, 'index.html')
 
 def get_jobs(request):
-  keyword = request.GET.get('keyword')
-  job_list = get_job_title(keyword)
-  context = {'job_list': job_list}
-  return render(request, 'main/home.html', context)
-  
+  if request.method == 'POST':
+    keyword = request.POST.get('keyword')
+    result_list = get_job_title(keyword)
+    context = {'result_list': result_list}
+    return render(request, 'main/home.html', context)
+  else:
+    return render(request, 'main/home.html')
